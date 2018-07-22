6fa4fc83dea77801f89c314f5c0e421d7a5b8510
Koen <koen-berendsen@hotmail.com>
2018-07-22 16:05:23 +0200

    indent to 4 tabs

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
 	@include('partials.page-header')
+	
 	@if (!have_posts())
 		<div class="alert alert-warning">
 			{{ __('Sorry, no results were found.', 'sage') }}
@@ -11,7 +12,7 @@
 
 	@while(have_posts()) @php the_post() @endphp
 		@include('partials.content-search')
-	@endwhile
+		@endwhile
 
 	{!! get_the_posts_navigation() !!}
 @endsection
